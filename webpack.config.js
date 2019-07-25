c9ac83ee1b0bde4280a17d6cc7c4809049db0690
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2019-07-25 21:33:19 +0300

    build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/todo-app/',
+    publicPath: './todo-app/dist',
     filename: 'build.js'
   },
   module: {
